6c090c09c2025ed71ff89c4ebc2d043afff9ac63
System Administrator <root@MacBook-Pro.local>
2018-04-16 14:06:46 -0400

    Stock Chart

--- dashEx.py
+++ dashEx.py
@@ -1,19 +1,37 @@
+import pandas_datareader.data as web
+import datetime
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash()
+stock = 'TSLA'
+start = datetime.datetime(2017, 1, 1)
+end = datetime.datetime(2018, 4, 15)
+df = web.DataReader(stock, 'morningstar', start, end)
+df.reset_index(inplace=True)
+df.set_index("Date", inplace=True)
+df = df.drop("Symbol", axis=1)
 
 app.layout = html.Div(children=[
-	html.H1('Dash Practice'),
-	dcc.Graph(id='example',
-		figure ={
-		'data': [
-				{'x':[1,2,3,4,5], 'y':[6,7,8,9,2], 'type':'line', 'name':'Grapphin'},
-				{'x':[3,4,8,3,4], 'y':[2,1,1,4,8], 'type':'line', 'name':'Numbers'},
-				]
-		})
-	])
+    html.H1(children='Oh look, a graph!'),
+
+    html.Div(children='''
+        Visualizing Stock!
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': df.index, 'y': df.Close, 'type': 'line', 'name': stock},
+            ],
+            'layout': {
+                'title': stock
+            }
+        }
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
